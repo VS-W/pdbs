da23b40ae051965e7aa4bb4ce52d2ecadcaa2b7a
VS-W <30299529+VS-W@users.noreply.github.com>
2023-05-31 23:29:35 -0400

    clean up pip messages on startup and bind only to localhost because docker bypasses ufw apparently

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt --upgrade
+pip install -r requirements.txt --upgrade --disable-pip-version-check | grep -v -E 'already satisfied|normal site-packages is not writeable'
 python3 -m bot.py
